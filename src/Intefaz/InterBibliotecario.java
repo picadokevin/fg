ab632896736da09db78e491337687b6c8ca412af
UsuarioPC <UsuarioPC@kevin>
2018-03-30 22:29:49 -0600

    interfaz de agregar usuarios en proceso

--- src/Intefaz/InterBibliotecario.java
+++ src/Intefaz/InterBibliotecario.java
@@ -6,6 +6,8 @@
 package Intefaz;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -18,10 +20,18 @@ public class InterBibliotecario {
     public GridPane interBibliotecario(){
      GridPane gpVentanaBibliotecario = new GridPane();
      Button btnAgregar = new Button("Agregar");
-     gpVentanaBibliotecario.add(btnAgregar, 0, 0);
-        
-        
-        
+     gpVentanaBibliotecario.add(btnAgregar, 0, 5);
+     Label nombreUnico=new Label("Nombre unico");
+     gpVentanaBibliotecario.add(nombreUnico, 0, 0);
+     TextField nombreUnic=new TextField();
+     gpVentanaBibliotecario.add(nombreUnic, 1, 0);
+     Label nombreCompleto=new Label("Nombre completo");
+     gpVentanaBibliotecario.add(nombreCompleto, 0, 1);
+      TextField nombreComplet=new TextField();
+       gpVentanaBibliotecario.add(nombreComplet, 1, 1);
+       Label TipoIdentificacion=new Label("Tipo de Identificación");
+     gpVentanaBibliotecario.add(TipoIdentificacion, 0, 2);
+       
          return gpVentanaBibliotecario;
     }
     
