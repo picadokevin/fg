77e516be1c61158f656ff338e27e6293582a2a69
Michelle <37647478+michipalma56@users.noreply.github.com>
2018-03-31 14:37:34 -0600

    Update Biblioteca.java

--- src/main/Biblioteca.java
+++ src/main/Biblioteca.java
@@ -40,6 +40,10 @@ public class Biblioteca extends Application {
         cbtipoUsuario.getItems().addAll("Usuario", "Autor", "Bibliotecario");
    
         Label lbUsuario = new Label("Usuario");
+        Label lbtipoID = new Label("Tipo de identificacion:");
+        ComboBox<String> cbtipoID = new ComboBox();
+        cbtipoID.getItems().addAll("Cedula", "Pasaporte");
+        Label lbIdentificacion = new Label("Identificacion");
         Label lbContraseña = new Label("Contraseña");
         Button btnConfirmar= new Button("Confirmar");
         btnConfirmar.setOnAction((event) -> {
@@ -52,15 +56,20 @@ public class Biblioteca extends Application {
         
         TextField tfUsuario = new TextField();
         TextField tfContraseña = new TextField();
+         TextField tfIdentificacion = new TextField();
 
         
         ventanaInicio.add(lbtipoUsuario, 0, 0);
         ventanaInicio.add(cbtipoUsuario, 1, 0);
         ventanaInicio.add(lbUsuario, 0, 1);
         ventanaInicio.add(tfUsuario, 1, 1);
-        ventanaInicio.add(lbContraseña, 0, 2);
-        ventanaInicio.add(tfContraseña, 1, 2);
-        ventanaInicio.add(btnConfirmar, 1, 3);
+        ventanaInicio.add(lbtipoID, 0, 2);
+        ventanaInicio.add(cbtipoID, 1, 2);
+        ventanaInicio.add(lbIdentificacion, 0, 3);
+        ventanaInicio.add(tfIdentificacion, 1, 3);
+        ventanaInicio.add(lbContraseña, 0, 4);
+        ventanaInicio.add(tfContraseña, 1, 4);
+        ventanaInicio.add(btnConfirmar, 0, 5);
         
         vbox.getChildren().addAll(lbBiblioteca, ventanaInicio);
         return vbox;
