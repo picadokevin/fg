dfb2db9b982fb0401b5c5f14afb1f088d826e48a
UsuarioPC <UsuarioPC@kevin>
2018-04-02 18:02:49 -0600

    se agrego tipo de usuario al constructor usuario ademas se esta haciendo un metodo para validar la entrada de usuario

--- src/domain/InterUsuario.java
+++ src/domain/InterUsuario.java
@@ -13,7 +13,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
-
+//hola
 /**
  *
  * @author Michelle
